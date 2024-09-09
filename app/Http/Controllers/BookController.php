f27eda69486e7316f9a3acae5677f30872e75b40
gigih31 <148758501+gigih31@users.noreply.github.com>
2024-09-09 08:31:30 +0700

    Initial commit

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,7 +11,7 @@ class BookController extends Controller
 {
     public function index(Request $request)
     {
-        $books = Book::paginate(10); 
+        $books = Book::paginate(2); 
 
         return response()->json($books);
     }
